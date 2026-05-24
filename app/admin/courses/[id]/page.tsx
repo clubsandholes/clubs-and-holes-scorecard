248b8ada7377961085f1a5c06e6708bd844aedf7
m329 <mikelopez73@gmail.com>
2026-05-23 22:30:00 -0700

    Add course manager course details

---
+++ app/admin/courses/[id]/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function CourseDetailPage() {
+  return (
+    <div className="min-h-screen bg-black p-6 text-white">
+      <div className="text-xs font-black uppercase tracking-[0.25em] text-[#ff9900]">
+        Clubs & Holes Admin
+      </div>
+
+      <h1 className="mt-2 text-4xl font-black">Course Detail</h1>
+
+      <p className="mt-2 text-gray-400">
+        Course info and 18-hole setup will live here.
+      </p>
+    </div>
+  );
+}
