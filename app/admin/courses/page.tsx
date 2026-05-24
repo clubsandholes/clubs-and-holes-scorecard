ce3d700b0c872e56549b3b0fede30de5ac781024
m329 <mikelopez73@gmail.com>
2026-05-23 22:14:41 -0700

    Add Course Admin Section

---
+++ app/admin/courses/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function CoursesAdminPage() {
+  return (
+    <div className="min-h-screen bg-black p-6 text-white">
+      <div className="text-xs font-black uppercase tracking-[0.25em] text-[#ff9900]">
+        Clubs & Holes Admin
+      </div>
+
+      <h1 className="mt-2 text-4xl font-black">Course Manager</h1>
+
+      <p className="mt-2 text-gray-400">
+        Reusable courses will live here.
+      </p>
+    </div>
+  );
+}
